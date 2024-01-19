ba78236d3e4a1b20aaa18a9dc70793426e36bd7b
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-01-19 13:37:35 +0530

    changing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello World</h1>);
+root.render(<h1>Hello world</h1>);
 
+//hello world wala cherkhar kar raha hu
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
