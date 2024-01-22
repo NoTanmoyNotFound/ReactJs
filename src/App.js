2499bf0f08a803408347e08520b0a92b376bcd47
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-01-22 21:26:20 +0530

    learning Jsx in React

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Tanmoy
-        </a>
-      </header>
-    </div>
-  );
+  function formatUser(user){
+    return user.firstname + " " + user.lastname;
+  }
+
+  function getGreeting(user) {
+    if (user){
+      return <h1>Hello, {formatUser(user)}</h1>
+    }
+    return <h1>Hello, Stranger!!!</h1>
+  }
+
+  const user = {
+    firstname: "Tanmoy",
+    lastname: "Das",
+  };
+  // const element = <h1>Hello, World and welcome to ReactJs</h1>
+  return <div>{getGreeting(user)}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello world</h1>);
+root.render(<App />);
 
-//hello world wala cherkhar kar raha hu
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
