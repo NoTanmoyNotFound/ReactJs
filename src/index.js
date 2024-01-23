7de58abcf504a18c5fcab8602dd08d142967565d
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-01-23 15:03:08 +0530

    Render Elemenet in React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, World</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    )
+    root.render(element);
+}
+
+setInterval(tick , 1000);
+
 
 
 // If you want to start measuring performance in your app, pass a function
